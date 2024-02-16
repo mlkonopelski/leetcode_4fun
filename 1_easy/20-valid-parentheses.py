9bccd2533551a87c9f802d8183083a46551fe5d7
Mateusz Konopelski <ml.konopelski@gmail.com>
2024-02-16 10:40:37 +0100

    success

--- 1_easy/20-valid-parentheses.py
+++ 1_easy/20-valid-parentheses.py
@@ -13,9 +13,32 @@ class Solution:
         
         return True if valid else False
     
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        
+        previous = ''
+        invalid = ['(]', '(}', '{)', '{]', '[)', '[}']
+        valid = ['()', '[]', '{}']
+        
+        for i, ch in enumerate(s):
+            if previous and previous[-1] + ch in invalid:
+                return False
+            elif previous and previous[-1] + ch in valid:
+                previous = previous[:-1]
+            else:
+                previous += ch
+        
+        return False if previous else True
+
+# PERFORMANCE
+# Runtime 35 ms (Beats 64.65% of users with Python)
+# Memory 16.83 MB (Beats 50.45% of users with Python)
+
     
 if __name__ == '__main__':
     
     assert Solution().isValid('()') == True
     assert Solution().isValid('()[]{}') == True
     assert Solution().isValid('(]') == False
+    assert Solution().isValid('([][()])') == True
