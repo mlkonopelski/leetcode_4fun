984146d9dd155a54d838d980143d3ab5d68b9939
panpluto <panpluto@gmail.com>
2024-04-06 18:57:53 +0200

    perfect user solution using recursive function

--- 2_medium/17_letter_combinations.py
+++ 2_medium/17_letter_combinations.py
@@ -163,7 +163,34 @@ class Solution:
 
 # BEST USER SOLUTION
 # NOTES: USe recursive function that starts from first digits and goes to next
-    
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+
+
+        LETTERS = {
+            '2': 'abc',
+            '3': 'def',
+            '4': 'ghi',
+            '5': 'jkl',
+            '6': 'mno',
+            '7': 'pqrs',
+            '8': 'tuv',
+            '9': 'wxyz'
+            }
+        
+        result = []
+
+        def backtrack(i, curr_str):
+            if len(curr_str) == len(digits):
+                result.append(curr_str)
+                return 
+            for c in LETTERS[digits[i]]:
+                backtrack(i+1, curr_str+c)
+
+        if digits:
+            backtrack(0, '')
+
+        return result
 
 if __name__ == '__main__':
 
